fb8da9640352104b1d7d2c30a1fec89f89ab7632
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-03-26 16:47:40 +0900

    초기 세팅

---
+++ groom/src/main/java/study/groom/GroomApplication.java
@@ -0,0 +1,13 @@
+package study.groom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroomApplication.class, args);
+	}
+
+}
---
+++ groom/src/test/java/study/groom/GroomApplicationTests.java
@@ -0,0 +1,13 @@
+package study.groom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
