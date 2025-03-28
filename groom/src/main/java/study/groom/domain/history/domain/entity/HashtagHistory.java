e32834225e262b8a69f8167d2b1a6b6ff299753e
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-03-28 17:49:29 +0900

    feat : yml 작성완료

--- groom/src/main/java/study/groom/domain/history/domain/entity/HashtagHistory.java
+++ groom/src/main/java/study/groom/domain/history/domain/entity/HashtagHistory.java
@@ -1,4 +1,25 @@
 package study.groom.domain.history.domain.entity;
 
-public class HashtagHistory {
+import jakarta.persistence.*;
+import lombok.*;
+import study.groom.domain.model.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class HashtagHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hashtag_id", nullable = false)
+    private Hashtag hashtag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "history_id", nullable = false)
+    private History history;
 }
